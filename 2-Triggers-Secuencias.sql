898174a51171a3719dd6c83f531062a729c764e0
Adrian <adriromerosoria@gmail.com>
2019-11-24 19:01:51 +0100

    Triggers de conbustible y almacen

--- 2-Triggers-Secuencias.sql
+++ 2-Triggers-Secuencias.sql
@@ -1,5 +1,30 @@
+DROP SEQUENCE sec_combustible;
+DROP SEQUENCE sec_almacen;
 DROP SEQUENCE sec_lineaCompra;
 DROP SEQUENCE sec_compra;
+
+CREATE SEQUENCE sec_combustible;
+
+CREATE OR REPLACE TRIGGER crea_id_combustible
+BEFORE INSERT ON combustibles
+REFERENCING NEW AS NEW 
+FOR EACH ROW 
+DECLARE valorSecuencia NUMBER := 0;
+BEGIN
+  SELECT sec_combustible.currval INTO valorSecuencia FROM DUAL;
+  :NEW.Id_COMB := valorSecuencia;
+END;  
+/
+
+CREATE SEQUENCE sec_almacen;
+
+CREATE OR REPLACE TRIGGER crea_id_almacen
+BEFORE INSERT ON Almacenes
+FOR EACH ROW
+BEGIN
+  SELECT sec_almacen.currval INTO :NEW.Id_A FROM DUAL;
+END;
+/
 
 CREATE SEQUENCE sec_lineaCompra;
 
