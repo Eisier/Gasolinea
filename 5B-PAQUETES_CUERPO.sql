45e8dc712b999cb184cd86d62b25167b404b0226
Albaro Chura Pascual <albarochura@gmail.com>
2019-11-27 19:30:43 +0100

    He añadido clientes y embarcaderos en paquetes_cuerpo.sql

--- 5B-PAQUETES_CUERPO.sql
+++ 5B-PAQUETES_CUERPO.sql
@@ -1,5 +1,90 @@
 -- PROCEDIMIENTOS - CUERPOS --
 
+--Clientes
+CREATE OR REPLACE PACKAGE BODY Pruebas_clientes IS
+
+  PROCEDURE inicializar
+  IS BEGIN
+    DELETE FROM clientes;
+  END inicializar;
+  
+  PROCEDURE insertar (nombre_prueba VARCHAR2, w_nombre IN clientes.nombre%TYPE, w_apellidos IN clientes.apellidos%TYPE,
+  w_dni IN clientes.dni%TYPE, w_telefono IN clientes.telefono%TYPE, w_correo IN clientes.correo%TYPE, w_fechaNacimiento IN clientes.fechaNacimiento%TYPE,
+  w_contraseña IN clientes.contraseña%TYPE, w_direccionImagen IN clientes.direccionImagen%TYPE, salida_Esperada BOOLEAN) IS
+    salida BOOLEAN := true;
+    cliente clientes%ROWTYPE;
+    w_Id_C NUMBER;
+    BEGIN
+      /*Insertar clientes*/ 
+      INSERT INTO clientes
+      VALUES (sec_cliente.nextval,w_nombre,w_apellidos,w_dni,w_telefono,w_correo,w_fechaNacimiento,w_contraseña,w_direccionImagen);
+      /*Seleccionar cliente y comprobar que los datos se insertaron correctamente*/
+      w_Id_C := sec_cliente.currval;
+      SELECT * INTO cliente FROM clientes WHERE Id_C = w_Id_C;
+      IF(cliente.nombre<>w_nombre OR cliente.apellidos<>w_apellidos OR cliente.dni<>w_dni OR cliente.telefono<>w_telefono OR cliente.correo<>w_correo 
+      OR cliente.fechaNacimiento<>w_fechaNacimiento OR cliente.contraseña<>w_contraseña OR cliente.direccionImagen<>w_direccionImagen) THEN
+        salida := false;
+      END IF;  
+    COMMIT WORK;
+    
+    /*Mostrar resultado de la prueba*/
+    DBMS_OUTPUT.put_line(nombre_prueba || ':' || ASSERT_EQUALS(salida,salida_Esperada));
+    
+    EXCEPTION
+    WHEN OTHERS THEN
+      DBMS_OUTPUT.put_line(nombre_prueba || ':' || ASSERT_EQUALS(false,salida_Esperada));
+      ROLLBACK;
+  END insertar;
+
+  PROCEDURE actualizar(nombre_prueba VARCHAR2, w_Id_C IN clientes.Id_C%TYPE, w_nombre IN clientes.nombre%TYPE, w_apellidos IN clientes.apellidos%TYPE,
+  w_dni IN clientes.dni%TYPE, w_telefono IN clientes.telefono%TYPE, w_correo IN clientes.correo%TYPE, w_fechaNacimiento IN clientes.fechaNacimiento%TYPE,
+  w_contraseña IN clientes.contraseña%TYPE, w_direccionImagen IN clientes.direccionImagen%TYPE, salida_Esperada BOOLEAN) IS
+    salida BOOLEAN := true;
+    cliente clientes%ROWTYPE;
+    BEGIN
+      /*Actualizar clientes*/
+      UPDATE clientes SET nombre = w_nombre, apellidos = w_apellidos, dni = w_dni, telefono = w_telefono, correo = w_correo, fechaNacimiento = w_fechaNacimiento, contraseña = w_contraseña, direccionImagen = w_direccionImagen
+      WHERE Id_C = w_Id_C;
+      /*Seleccionar cliente y comprobar que los campos se actualizan correctamente*/
+      SELECT * INTO cliente FROM clientes WHERE Id_C = w_Id_C;
+      IF (cliente.nombre<>w_nombre OR cliente.apellidos<>w_apellidos OR cliente.dni<>w_dni OR cliente.telefono<>w_telefono OR cliente.correo<>w_correo 
+      OR cliente.fechaNacimiento<>w_fechaNacimiento OR cliente.contraseña<>w_contraseña OR cliente.direccionImagen<>w_direccionImagen) THEN
+        salida := false;
+      END IF;
+      COMMIT WORK;
+      
+      /*Mostrar resultado de la prueba*/
+      DBMS_OUTPUT.put_line(nombre_prueba || ':' || ASSERT_EQUALS(salida,salida_Esperada));
+      
+      EXCEPTION
+      WHEN OTHERS THEN
+        DBMS_OUTPUT.put_line(nombre_prueba || ':' || ASSERT_EQUALS(false,salida_Esperada));
+   END actualizar;
+   
+   PROCEDURE eliminar(nombre_prueba VARCHAR2, w_Id_C IN CLIENTES.ID_C%TYPE, salida_Esperada BOOLEAN) IS
+    salida BOOLEAN := true;
+    n_clientes INTEGER;
+    BEGIN
+      /*Eliminar clientes*/
+      DELETE FROM clientes WHERE Id_C=w_Id_C;
+      /*Verificar que el clientes no se encuentra en la BD*/
+      SELECT COUNT(*) INTO n_clientes FROM clientes WHERE Id_C=w_Id_C;
+      IF(n_clientes<>0) THEN
+        salida := false;
+      END IF;
+      COMMIT WORK;
+      
+      /*Mostrar resultado de la prueba*/
+      DBMS_OUTPUT.put_line(nombre_prueba || ':' || ASSERT_EQUALS(salida,salida_Esperada));
+      
+      EXCEPTION
+      WHEN OTHERS THEN
+        DBMS_OUTPUT.put_line(nombre_prueba || ':' || ASSERT_EQUALS(false,salida_Esperada));
+        ROLLBACK;
+   END eliminar;     
+END Pruebas_clientes;  
+/
+
 --Trabajadores
 CREATE OR REPLACE PACKAGE BODY pruebas_trabajadores AS
 
@@ -140,6 +225,113 @@ WHEN OTHERS THEN
 END eliminar;
 
 END pruebas_trabajadores;
+/
+
+--Embarcaderos
+CREATE OR REPLACE PACKAGE BODY pruebas_embarcaderos AS
+
+PROCEDURE inicializar AS
+BEGIN
+    /* Borrar contenido de la tabla */
+    DELETE FROM embarcaderos;
+END inicializar;
+
+
+PROCEDURE insertar
+ (nombre_prueba      VARCHAR2,
+ w_disponible        IN embarcaderos.disponible%TYPE,
+ salidaEsperada      BOOLEAN)
+AS
+
+salida BOOLEAN := true;
+embarcadero embarcaderos%ROWTYPE;
+w_id_E INTEGER;
+
+BEGIN
+    
+    /* Insertar embarcadero */
+    INSERT INTO embarcaderos 
+    VALUES(sec_embarcadero.nextval, w_disponible);
+    
+    /* Seleccionartrabajador y comprobar que los datos se insertan correctamente */
+    w_id_E := sec_embarcadero.currval;
+    SELECT * INTO embarcadero FROM embarcaderos WHERE id_E=w_id_E;
+    IF (embarcadero.disponible<>w_disponible) THEN
+        salida := false;
+    END IF;
+    COMMIT WORK;
+    
+/* Mostrar resultados de la prueba */
+DBMS_OUTPUT.put_line(nombre_prueba || ':' || ASSERT_EQUALS(salida, salidaEsperada));
+EXCEPTION
+WHEN OTHERS THEN 
+    DBMS_OUTPUT.put_line(nombre_prueba || ':' || ASSERT_EQUALS(false, salidaEsperada));
+    ROLLBACK;
+END insertar;
+
+
+
+PROCEDURE actualizar
+ (nombre_prueba       VARCHAR2,
+  w_id_E              IN embarcaderos.id_E%TYPE,
+  w_disponible        IN embarcaderos.disponible%TYPE,
+  salidaEsperada      BOOLEAN)
+AS
+
+salida BOOLEAN := true;
+embarcadero embarcaderos%ROWTYPE;
+
+BEGIN
+    
+    /* Actualizar trabajador */
+    UPDATE embarcaderos SET disponible=w_disponible            WHERE id_E=w_id_E;
+    
+    /* Seleccionar trabajador y comprobar que los campos se han actualizado correctamente */
+    SELECT * INTO embarcadero FROM embarcaderos WHERE id_E=w_id_E;
+    IF(embarcadero.disponible<>w_disponible) THEN
+      salida := false;
+    END IF;
+    COMMIT WORK;
+    
+/* Mostrar resultados de la prueba */
+DBMS_OUTPUT.put_line(nombre_prueba || ':' || ASSERT_EQUALS(salida, salidaEsperada));
+EXCEPTION
+WHEN OTHERS THEN 
+    DBMS_OUTPUT.put_line(nombre_prueba || ':' || ASSERT_EQUALS(false, salidaEsperada));
+    ROLLBACK;
+END actualizar;
+
+
+PROCEDURE eliminar
+ (nombre_prueba       VARCHAR2,
+  w_id_E              IN embarcaderos.id_E%TYPE,
+  salidaEsperada      BOOLEAN)
+AS
+
+salida BOOLEAN := true;
+n_embarcaderos INTEGER;
+
+BEGIN
+
+    /* Eliminar empleado */
+    DELETE FROM embarcaderos WHERE id_E=w_id_E;
+    
+    /* Verificar que el trabajador no se encuentra en la BD */
+    SELECT COUNT (*) INTO n_embarcaderos FROM embarcaderos WHERE id_E=w_id_E;
+    IF(n_embarcaderos<>0) THEN
+      SALIDA := false;
+    END IF;
+    COMMIT WORK;
+    
+/* Mostrar resultados de la prueba */
+DBMS_OUTPUT.put_line(nombre_prueba || ':' || ASSERT_EQUALS(salida, salidaEsperada));
+EXCEPTION
+WHEN OTHERS THEN 
+    DBMS_OUTPUT.put_line(nombre_prueba || ':' || ASSERT_EQUALS(false, salidaEsperada));
+    ROLLBACK;
+END eliminar;
+
+END pruebas_embarcaderos;
 /
 
 --Proveedores
